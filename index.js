707c7ae1b8556949f5f07322d05077e16bff6f89
Tazwar <tazwar9t6@gmail.com>
2020-10-14 17:23:02 +0600

    server updated and 70% done

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 const uri = "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/creative-agency?retryWrites=true&w=majority";
 app.use(cors());
 app.use(bodyParser.json());
+app.use(fileUpload());
 const port = 5000;
 
 
@@ -54,6 +56,43 @@ app.post("/addOrder", (req, res) => {
   });
 });
 
+
+app.post("/addReview", (req, res) => {
+  const review = req.body;
+  reviewCollection.insertOne(review)
+  .then((result) => {
+    res.send(result.insertedCount > 0);
+  });
+});
+
+app.post("/addService", (req, res) => {
+  const file = req.files.file;
+  const title = req.body.title;
+  const description = req.body.description;
+
+  const newImg = req.files.file.data;
+  const encImg = newImg.toString("base64");
+
+  var img = {
+    contentType: req.files.file.mimetype,
+    size: req.files.file.size,
+    img: Buffer.from(encImg, "base64"),
+  };
+
+  serviceCollection
+    .insertOne({ title, description, img })
+    .then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+});
+
+app.post("/addAdmin", (req, res) => {
+  const adminEmail = req.body;
+  adminCollection.insertOne(adminEmail)
+  .then((result) => {
+    res.send(result.insertedCount > 0);
+  });
+});
 
   });
   app.get("/", (req, res) => {
